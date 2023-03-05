6700453dc5b31f27726e0e910f704e02be8beea3
Michael ALTMAN <altman_mike@yahoo.fr>
2023-03-05 14:46:11 +0100

    service data pour nous, avec plus de fonctions, récup json à faire

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -9,8 +9,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DataController extends AbstractController
-{
-    #[Route('/data-get-from-api', name: 'app_dataGetFromAPI')]
+{        
+    #[Route('/data', name: 'app_dataHome')]
+    public function index(): Response
+    {
+        return $this->render('data/index.html.twig', [
+            'controller_name' => 'DataController'
+        ]);
+    } 
+
+    #[Route('/data-get-json', name: 'app_dataGetJsonFromAPI')]
+    public function dateGetJsonFromAPI(): Response
+    {
+        // TODO: Récupérer les données de l'API MOOC au format .json  à placer dans public/js/ pour l'instant en local
+        return $this->redirectToRoute('app_dataJsonFile'); 
+    } 
+
+    #[Route('/data-set-from-api', name: 'app_dataSetFromAPI')]
     public function dataGetFromAPI(FetchDataFromAPIService $fetchDataFromAPIService): Response
     {
         $result = '';
@@ -34,7 +49,7 @@ class DataController extends AbstractController
         try {
             $resetDataFromAPIService->resetTablesAndIncrements();
             $fetchDataFromAPIService->getDataFromAPI();
-            $result = "La base de donnée a correctement été mise à jour par l'API";
+            $result = "La base de données a correctement été mise à jour par l'API";
         } catch(\Exception $e) {
             $result = $e->getMessage();
         }        
@@ -45,11 +60,37 @@ class DataController extends AbstractController
         ]);
     }
 
-    #[Route('/data', name: 'app_dataHome')]
-    public function index(): Response
+    #[Route('/data-reset-fast', name: 'app_dataResetFast')]
+    public function dataResetFast(ResetDataFromAPIService $resetDataFromAPIService, FetchDataFromAPIService $fetchDataFromAPIService): Response
     {
-        return $this->render('data/index.html.twig', [
-            'controller_name' => 'DataController'
+        $result = '';
+        try {
+            $resetDataFromAPIService->resetTablesAndIncrements();            
+            $result = "Les données de la base ont correctement été effacées et réinitialisées";
+        } catch(\Exception $e) {
+            $result = $e->getMessage();
+        }        
+
+        return $this->render('data/update.html.twig', [
+            'controller_name' => 'DataController',
+            'result' => $result
         ]);
-    }    
+    }
+
+    #[Route('/data-reset-users', name: 'app_dataResetUsers')]
+    public function dataResetUsers(ResetDataFromAPIService $resetDataFromAPIService, FetchDataFromAPIService $fetchDataFromAPIService): Response
+    {
+        $result = '';
+        try {
+            $resetDataFromAPIService->resetUsersTablesAndIncrements();            
+            $result = "Les données de la table Users ont correctement été effacées et réinitialisées";
+        } catch(\Exception $e) {
+            $result = $e->getMessage();
+        }        
+
+        return $this->render('data/update.html.twig', [
+            'controller_name' => 'DataController',
+            'result' => $result
+        ]);
+    }   
 }
--- src/Service/ResetDataFromAPIService.php
+++ src/Service/ResetDataFromAPIService.php
@@ -52,6 +52,41 @@
                 $stm->execute();
 
                 // Le reste sera fait dans le controller directement (il utilise l'autre service)
+                // Et on épargne la table Users :o)
+
+            } catch(PDOException $e) {
+                die("Connexion MySQL local failed: " . $e->getMessage());
+            }           
+        }
+
+        public function resetUsersTablesAndIncrements() {
+
+            // Paramétrage local MySQL
+            $host = 'localhost';
+            $dbname = 'apiplatform';
+            $dbUser = 'root';
+            $dbPass = '';
+
+            // Paramétrage hébergeur perso MySQL
+            //$host = '185.98.131.93';
+            //$dbname = 'devin1226832_5v8lu';
+            //$dbUser = 'devin1226832_5v8lu';
+            //$dbPass = 'lfwiwdyo6l';
+            
+            try {
+                // Connexion à la bdd:            
+                $pdo = new PDO("mysql:host=$host; dbname=$dbname", $dbUser, $dbPass);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Suppression de toutes les données de la table des utilisateurs (revendeurs et clients)
+                $sqlU = 'DELETE FROM `users`';
+                $stm = $pdo->prepare($sqlU);
+                $stm->execute();
+
+                // Remise à zéro des incrémentation d'id
+                $sqlU = 'ALTER TABLE `customers` AUTO_INCREMENT = 0';
+                $stm = $pdo->prepare($sqlU);
+                $stm->execute();
 
             } catch(PDOException $e) {
                 die("Connexion MySQL local failed: " . $e->getMessage());
